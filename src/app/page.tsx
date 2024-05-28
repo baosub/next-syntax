bba3ed6b380f0bbf5ca93198b47fad43e89ef82d
Baldo <barrosoub@protonmail.com>
2024-05-27 21:17:54 -0600

    linking subjtect is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
+
+
+
 
 const Home = () => {
+  const router = useRouter()
   return (
     <>
-    <h2>Home</h2>
-      
+      <h2>Home</h2>
+      <button type="button" onClick={() => router.push('/dashboard')}>
+        Dashboard
+      </button>
+
     </>
   )
 }
